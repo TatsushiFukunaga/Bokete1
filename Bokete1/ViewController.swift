2259a5163505726779511120b1263236b0d36396
tatsushi.fukunaga <tatsushi1789@gmail.com>
2020-09-20 11:08:44 +0900

    Initial Commit

---
+++ Bokete1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bokete1
+//
+//  Created by Tatsushi Fukunaga on 2020/09/20.
+//  Copyright © 2020 tatsushi.fukunaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
